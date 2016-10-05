154c4fa55dd00776aa266fd49d71dd038ee028d2
Saydul Bashar <tamimnoone@gmail.com>
2016-10-05 13:37:31 +0000

    Added steps to install nodejs.

--- setup_ubuntu_system.sh
+++ setup_ubuntu_system.sh
@@ -35,6 +35,12 @@ echo "apache2"
 sudo apt-get install -y apache2
 echo done
 
+echo "nodejs"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install build-essential
+echo done
+
 echo done
 
 echo "Setup gitconfig"
